c60f3ee93094e3ff63a02fd0265bc49fbac40d69
olegserbat <olegserbat@yandex.ru>
2024-02-17 21:11:53 +0300

    lesson2 tests

--- test/StringUtilsTest.php
+++ test/StringUtilsTest.php
@@ -6,12 +6,12 @@ require_once ('../vendor/autoload.php');
 
 use function StringUtils\capitalize;
 
-if (capitalize('hello') !== 'Hello') {
-    throw new \Exception('Функция работает неверно!');
-}
+use Webmozart\Assert\Assert;
 
-if (capitalize('') !== '') {
-    throw new \Exception('Функция работает неверно!');
-}
+Assert::eq(capitalize(''), '');
+// Первый параметр actual – то, что пришло
+// Второй параметр expected – то, что ожидает тест
+// Правильный порядок аргументов имеет большое значение при анализе ошибки
+Assert::eq(capitalize('hello'), 'Hello');
 
 echo 'Все тесты пройдены!';
